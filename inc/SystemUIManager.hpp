8fdd7df85d10e111f7ff114c9b5db26cd5790c7f
Ruslan Kopanev <rakopanev@vk.com>
2025-01-24 23:22:52 +0300

    --

--- inc/SystemUIManager.hpp
+++ inc/SystemUIManager.hpp
@@ -3,10 +3,15 @@
 class SystemUIManager
 {
 private:
-
+    bool m_startUpState = false;
 public:
     SystemUIManager();
-    ~SystemUIManager();
+    ~SystemUIManager() = default;
+
+    void startUp();
+    void shutDown();
 
     static void _bind_methods();
+
+    void _process(double delta);
 };
--- src/SystemUIManager.cpp
+++ src/SystemUIManager.cpp
@@ -4,10 +4,23 @@ SystemUIManager::SystemUIManager()
 {
 }
 
-SystemUIManager::~SystemUIManager()
+void SystemUIManager::_bind_methods()
 {
 }
 
-void SystemUIManager::_bind_methods()
+void _bind_methods()
 {
+}
+
+void SystemUIManager::startUp()
+{
+    m_startUpState = true;
+}
+
+void SystemUIManager::shutDown()
+{
+    if (m_startUpState == true)
+    {
+        m_startUpState = false;
+    }
 }
